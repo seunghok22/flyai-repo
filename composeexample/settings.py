ad5208173dfe3d930a4c84082d1f87cbc2b2eeaa
seunghok22 <seunghok22@gmail.com>
2025-02-23 13:05:09 +0900

    set

--- composeexample/settings.py
+++ composeexample/settings.py
@@ -15,7 +15,7 @@ import os
 from datetime import timedelta
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
-BASE_DIR = Path(__file__).resolve().parent.parent
+BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
 AUTH_USER_MODEL = 'user.User'
 # Quick-start development settings - unsuitable for production
